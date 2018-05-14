0581f3285fc5f477ffb0491734b74708098af2b6
Adrien Catel <adrien.catel@nist.gov>
2018-05-14 10:11:42 -0400

    feat: data status api
    Change-Id: Ibabb236b05f230bc38ef21747134607663b89845

---
+++ core_main_registry_app/commons/constants.py
@@ -0,0 +1,8 @@
+""" all constants for the core_main_registry_app
+"""
+
+
+class DataStatus:
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
+    DELETED = 'deleted'
---
+++ core_main_registry_app/components/data/api.py
@@ -0,0 +1,44 @@
+""" Data's registry api
+"""
+
+import core_main_app.components.data.api as data_api
+from core_main_app.commons import exceptions as exceptions
+from xml_utils.xsd_tree.xsd_tree import XSDTree
+
+
+def set_status(data, status, user):
+    """ Set the status of a data
+
+    Args:
+        data:
+        status:
+        user:
+
+    Returns: Data
+
+    """
+    # build the xsd tree
+    xml_tree = XSDTree.build_tree(data.xml_content)
+    # get the root
+    root = xml_tree.getroot()
+    # and change the attribute
+    root.attrib['status'] = status
+    # update the xml content
+    data.xml_content = XSDTree.tostring(xml_tree)
+    # upsert the data
+    return data_api.upsert(data, user)
+
+
+def get_status(data):
+    """ Get the status saved in the data's dict content
+
+    Args:
+        data:
+
+    Returns:
+
+    """
+    try:
+        return data.dict_content['Resource']['@status']
+    except Exception, e:
+        raise exceptions.ModelError(e.message)
