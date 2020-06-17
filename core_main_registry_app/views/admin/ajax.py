c6a9c17e7db5bca4c7312c3e1df6f26df078e400
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-06-17 15:59:51 -0400

    fix: avoid returning unescaped messages
    Change-Id: I90987e412d3f51114d24572b12bb96dcf75a4b99

--- core_main_registry_app/views/admin/ajax.py
+++ core_main_registry_app/views/admin/ajax.py
@@ -2,9 +2,10 @@
     Admin ajax
 """
 from django.http.response import HttpResponse, HttpResponseBadRequest
+from django.utils.html import escape
 
-from core_main_registry_app.components.version_manager import api as version_manager_api
 from core_main_app.components.template import api as template_api
+from core_main_registry_app.components.version_manager import api as version_manager_api
 
 
 def set_current_template_version_from_version_manager(request):
@@ -19,6 +20,8 @@ def set_current_template_version_from_version_manager(request):
     try:
         version_manager_api.set_current(template_api.get(request.GET["id"]))
     except Exception as e:
-        return HttpResponseBadRequest(str(e), content_type="application/javascript")
+        return HttpResponseBadRequest(
+            escape(str(e)), content_type="application/javascript"
+        )
 
     return HttpResponse(content_type="application/javascript")
